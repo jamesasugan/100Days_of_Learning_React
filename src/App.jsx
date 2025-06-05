53c65216cfa9114a10e38a1403465652784cf49d
james <jjasugan@gmail.com>
2025-06-05 13:11:56 +0800

    Day 3: Create your first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Profile from './components/Profile.jsx';
 
 function App() {
   const name = "James Asugan";
@@ -6,10 +7,8 @@ function App() {
   return (
     
     <>
-      <div className='bg-gray-500 w-full h-screen flex flex-col items-center justify-center text-2xl'>
-        <h1>Hi, {name}</h1>
-        <p>I love coding in {programmingLanguage}</p>
-        <p>{programmingLanguage === 'JavaScript' ? "That's a great choice!" : `Nice! ${programmingLanguage} is cool too.`}</p>
+      <div>
+        <Profile />
       </div>
     </>
   )
