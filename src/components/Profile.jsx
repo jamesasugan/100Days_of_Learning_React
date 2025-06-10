1f2595d23e27dd3990cf8948084abcca33ed5928
james <jjasugan@gmail.com>
2025-06-10 08:55:36 +0800

    Day 5: Props (intro)

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,10 @@
-const Profile = () => {
+const Profile = (props) => {
   return (
     <div className='w-full flex flex-col items-center justify-center bg-gray-300'>
         <div className='bg-blue-500 text-white p-6 rounded-lg shadow-lg text-lg'>
-            <h2>James Asugan</h2>
-            <p>I love playing games on my free time.</p>
+            <h2>Hi, my name is {props.name}</h2>
+            <p>I am {props.age} years old.</p>
+            <p>My hobby is {props.hobby}</p>
         </div>
     </div>
   )
