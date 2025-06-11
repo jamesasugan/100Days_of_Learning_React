4a7873adeb74099607cf1d4d462ffcf02b8c92a5
james <jjasugan@gmail.com>
2025-06-11 08:35:32 +0800

    Day 6: Destructure props

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,10 @@
-const Profile = (props) => {
+const Profile = ({name, age, hobby}) => {
   return (
     <div className='w-full flex flex-col items-center justify-center bg-gray-300'>
         <div className='bg-blue-500 text-white p-6 rounded-lg shadow-lg text-lg'>
-            <h2>Hi, my name is {props.name}</h2>
-            <p>I am {props.age} years old.</p>
-            <p>My hobby is {props.hobby}</p>
+            <h2>Hi, my name is {name}</h2>
+            <p>I am {age} years old.</p>
+            <p>My hobby is {hobby}</p>
         </div>
     </div>
   )
