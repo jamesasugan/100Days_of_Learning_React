62694d2c0f6c4a6ce70c1da0a97c945dd2a43396
james <jjasugan@gmail.com>
2025-06-22 22:29:13 +0800

    Day 12: Add and subtract from counter using buttons.

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -8,19 +8,49 @@ const Counter = () => {
             <div className='w-[20%]'>
                 <p className='text-lg font-semibold'>Count: <span className='ml-1 '>{count}</span></p>
                 <div className='flex flex-col p-0'>
-                    <button onClick={() => setCount(count + 1)} className='p-3 text-white bg-green-500 text-lg font-semibold rounded-lg mt-5 hover:bg-green-600 transition duration-150 cursor-pointer'>Count Up</button>
+                    <button onClick={() => setCount(prev => prev + 1)} className='p-3 text-white bg-green-500 text-lg font-semibold rounded-lg mt-5 hover:bg-green-600 transition duration-150 cursor-pointer'>+1</button>
                     <button onClick={() => {
                         if(count > 0){
-                            setCount(count - 1);
+                            setCount(prev => prev -1);
                             setMessage('');
                         }else{
-                            setMessage("Oops, you have reached the limit.");
+                            setMessage("Cannot go below 0");
                             setTimeout(() => {
                                 setMessage("");
                             }, (2000));
                         }
                     }}
-                    className='p-3 text-white bg-red-500 text-lg font-semibold rounded-lg mt-5 hover:bg-red-600 transition duration-150 cursor-pointer'>Count Down</button>
+                    className='p-3 text-white bg-red-500 text-lg font-semibold rounded-lg mt-5 hover:bg-red-600 transition duration-150 cursor-pointer'>-1</button>
+
+                     <button onClick={() => setCount(prev => prev + 5)} className='p-3 text-white bg-green-500 text-lg font-semibold rounded-lg mt-5 hover:bg-green-600 transition duration-150 cursor-pointer'>+5</button>
+
+                     <button onClick={() => {
+                        setCount(prev => {
+                            const newCount = prev * 2;
+                            if(newCount > 50){
+                                setMessage("Whoa! That's a huge number!");
+                                setTimeout(() => {
+                                setMessage("");
+                            }, (2000));
+                            }
+                            return newCount;
+                        })    
+                    }
+                        
+                     } className='p-3 text-white bg-green-500 text-lg font-semibold rounded-lg mt-5 hover:bg-green-600 transition duration-150 cursor-pointer'>x2</button>
+
+                    <button onClick={() => {
+                        if(count >= 5){
+                            setCount(prev => prev -5);
+                            setMessage('');
+                        }else{
+                            setMessage("Cannot go below 0");
+                            setTimeout(() => {
+                                setMessage("");
+                            }, (2000));
+                        }
+                    }}
+                    className='p-3 text-white bg-red-500 text-lg font-semibold rounded-lg mt-5 hover:bg-red-600 transition duration-150 cursor-pointer'>-5</button>
 
                     <button onClick={() => setCount(0)} className='p-3 text-white bg-gray-500 text-lg font-semibold rounded-lg mt-5 hover:bg-gray-600 transition duration-150 cursor-pointer'>Clear</button>
 
