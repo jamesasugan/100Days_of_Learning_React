f6d2dc6dafdfb556e069a135dbd8158d5e8014df
james <jjasugan@gmail.com>
2025-06-27 00:54:12 +0800

    Day 14: Multiple form fields (Added password field)

--- src/components/NewForm.jsx
+++ src/components/NewForm.jsx
@@ -3,7 +3,8 @@ import { useState } from 'react';
 const NewForm = () => {
     const [formData, setFormData] = useState({
         name: '',
-        email: ''
+        email: '',
+        password: ''
     });
 
     const handleChange = (e) => {
@@ -20,8 +21,10 @@ const NewForm = () => {
                 <div className="bg-gray-200 p-5 rounded flex flex-col space-y-4 w-[30%]">
                     <input type="text" name='name' value={formData.name} onChange={handleChange} className='bg-white text-black p-2 rounded-sm' placeholder='Name'/>
                     <input type="email" name='email' value={formData.email} onChange={handleChange} className='bg-white text-black p-2 rounded-sm' placeholder='Email'/>
+                    <input type="password" name='password' value={formData.password} onChange={handleChange} className='bg-white text-black p-2 rounded-sm' placeholder='Password'/>
                     <p className='font-medium'>Hello, {formData.name || "Stranger"}</p>
                     <p className='font-medium'>Your email is: {formData.email || "None"}</p>
+                    <p className='font-medium'>Your password is: {formData.password || "None"}</p>
                 </div>
             </section>
     )
