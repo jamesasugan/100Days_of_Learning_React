06e9765033c068d2e4f84e65577fef55d1d540be
james <jjasugan@gmail.com>
2025-08-27 13:58:18 +0800

    Day 39: Textarea + Select (controlled)

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,16 +1,21 @@
 import { useState } from "react";
 
 const Contact = () => {
-  const [message, setMessage] = useState("");
+  const [successMessage, setsuccessMessage] = useState("");
   const [submittedData, setSubmittedData] = useState(null);
   const [errors, setErrors] = useState({});
-  const [formData, setFormData] = useState({
+  
+  const initialFormState = {
     subject: "",
     name: "",
     email: "",
     message: "",
+    country: "",
+    comment: "",
     subscribe: false,
-  });
+  };
+
+  const [formData, setFormData] = useState(initialFormState);
 
   const handleChange = (e) => {
   const { name, value, type, checked } = e.target;
@@ -29,8 +34,9 @@ const Contact = () => {
     } else if(!/\S+@\S+\.\S+/.test(formData.email)) {
       newErrors.email = "Email is invalid";
     }
-    
     if(!formData.message.trim()) newErrors.message = "Message is required";
+    if(!formData.country.trim()) newErrors.country = "Country is required";
+    if(!formData.comment.trim()) newErrors.comment = "Comment is required";
     return newErrors;
   }
   
@@ -44,11 +50,11 @@ const Contact = () => {
     setErrors({});
     console.log(formData);
     setSubmittedData(formData);  
-    setMessage("Form Submitted!");
+    setsuccessMessage("Form Submitted!");
     setTimeout(() => {
-      setMessage(""); 
+      setsuccessMessage(""); 
       setSubmittedData(null);
-      setFormData({subject: "", name: "", email: "", message: "", subscribe: false});
+      setFormData(initialFormState);
     }, 3000);
 
     
@@ -79,9 +85,23 @@ const Contact = () => {
           {errors.message && <p className="text-red-500 text-sm">{errors.message}</p>}
         </fieldset>
         <fieldset className="fieldset">
+          <label htmlFor="country" className="fieldset-legend text-gray-800 text-base">What is your country?</label>
+          <select value={formData.country} name="country" id="country" autoComplete="none" onChange={handleChange} className="select bg-gray-200">
+            <option disabled={true}>Select a country</option>
+            <option value="Philippines">Philippines</option>
+            <option value="USA">USA</option>
+            <option value="Canada">Canada</option>
+          </select>
+          {errors.country && <p className="text-red-500 text-sm">{errors.country}</p>}
+        </fieldset>
+        <fieldset className="fieldset">
+          <label htmlFor="comment" className="fieldset-legend text-gray-800 text-base">What is your comment?</label>
+          <textarea id="comment" className="textarea h-24 bg-gray-200" autoComplete="none" value={formData.comment} onChange={handleChange} name="comment" placeholder="Bio"></textarea>
+          {errors.comment && <p className="text-red-500 text-sm">{errors.comment}</p>}
+        </fieldset>
+        <fieldset className="fieldset">
           <label htmlFor="subscribe" className="fieldset-legend text-gray-800 text-base">Subscribe to newsletter?</label>
-          <input type="checkbox" id="subscribe" name="subscribe" checked={formData.subscribe} 
-          onChange={(e) => setFormData(prev => ({...prev, subscribe: e.target.checked}))} 
+          <input type="checkbox" id="subscribe" name="subscribe" checked={formData.subscribe} onChange={handleChange}
           className="checkbox checkbox-info" />
         </fieldset>
         <button type="submit" className="btn btn-success">Submit</button>
@@ -89,18 +109,20 @@ const Contact = () => {
       
       {submittedData && (
         <div className="p-2 bg-gray-200 rounded mt-5 text-base space-y-1">
-          {message && (
+          {successMessage && (
             <div role="alert" className="alert alert-success">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
-              <span>{message}</span>
+              <span>{successMessage}</span>
             </div>
           )}
           <p><strong>Subject: </strong>{submittedData.subject}</p>
           <p><strong>Name: </strong>{submittedData.name}</p>
           <p><strong>Email: </strong>{submittedData.email}</p>
           <p><strong>Message: </strong>{submittedData.message}</p>
+          <p><strong>Country: </strong>{submittedData.country}</p>
+          <p><strong>Comment: </strong>{submittedData.comment}</p>
           <p><strong>Subscribe: </strong>{submittedData.subscribe ? "Yes" : "No"}</p>
         </div>
       )}
